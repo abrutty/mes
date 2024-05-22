3310dc6f0d0e59c412aabd33d888969d5ce8b199
“sunsetshimmer” <244758462@qq.com>
2024-05-22 14:36:27 +0800

    c5过程检验出来老李和Austny完成合并 开始检测模板 待检任务完成

---
+++ mes-cpp/mes-c5-QualityControl/controller/awaitinspection/OaslController.cpp
@@ -0,0 +1,8 @@
+#include "stdafx.h"
+#include "OaslController.h"
+#include "../ApiDeclarativeServicesHelper.h"
+
+OaslPageJsonVO::Wrapper OaslController::execQuerySample(const OaslQuery::Wrapper& query, const PayloadDTO& payload)
+{
+	return {};
+}
